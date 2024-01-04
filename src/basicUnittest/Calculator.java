1e52ade97eac1760f10419431df53ac3cff4a179
Worachet Uttha <wuttha@webmail.npru.ac.th>
2024-01-04 17:06:53 +0700

    first commit

---
+++ src/basicUnittest/Calculator.java
@@ -0,0 +1,27 @@
+package basicUnittest;
+
+public class Calculator {
+private int num1;
+private int num2;
+private int result;
+
+public int add() {
+	result = num1 + num2;
+	return result;
+}
+
+public void multiply() {
+	result = num1 * num2;
+	
+}
+
+public int showResult() {
+	
+	return result;
+}
+
+public void setValue(int num1, int num2) {
+	this.num1 = num1;
+	this.num2 = num2;
+}
+}
